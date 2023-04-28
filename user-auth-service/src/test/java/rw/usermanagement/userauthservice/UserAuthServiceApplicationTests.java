06d1ea61da3a6a92897bb0a655601062f5a5c2fd
Roger NDUTIYE <rogerndutiye@gmail.com>
2023-04-28 12:15:07 +0200

    Project Setup

---
+++ user-auth-service/src/test/java/rw/usermanagement/userauthservice/UserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.usermanagement.userauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
